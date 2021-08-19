14d8f6e901e5d605ebc25dc52f84e8600a63fc75
David Hdez <david.hdezalv29@gmail.com>
2021-08-19 13:55:29 -0500

    Creamos la clase DataContext con metodos constructor y OnModelCreating

---
+++ ProyectoECI.Web/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿namespace ProyectoECI.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using ProyectoECI.Web.Data.Entities;
+    using System.Linq;
+
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //Habilitar mi comportamiento de eliminado en cascada. 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            var cascadeFKs = builder.Model
+                .GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            base.OnModelCreating(builder);
+        }
+    }
+}
